0c2687f3d3f8349de57d6d14088ab6a76601182e
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-03-07 10:02:10 +0530

    Add Initial Files

---
+++ models/review.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const { number } = require("joi");
+const Schema = mongoose.Schema;
+
+//defining review schema
+const reviewSchema = new Schema({
+  comment: String,
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+  },
+  createdAT: {
+    type: Date,
+    default: Date.now(),
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+module.exports = mongoose.model("Review", reviewSchema);
